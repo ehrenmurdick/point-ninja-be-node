3a81cdee7950934d6f808bf022ac293da27f80aa
Ehren Murdick <emurdick@pivotal.io>
2017-01-27 16:31:36 -0800

    wip

--- index.js
+++ index.js
@@ -1,89 +1,44 @@
 let ws = require('nodejs-websocket')
-let redis = require('redis')
 const _ = require('lodash')
 
-let client = redis.createClient()
+let socketPool = []
 
-let socketPool = {}
-
-const joinAction = (conn, action) => {
-  if (_.isNil(socketPool[action.uuid])) {
-    socketPool[action.uuid] = []
-  }
-  socketPool[action.uuid] = _.concat(socketPool[action.uuid], conn)
-  console.log(conn.key+' joined '+action.uuid)
-  console.log(socketPool.length+' active connections')
+const joinParty = (conn, action) => {
+  conn.userId = action.userId
+  conn.partyId = action.uuid
+  socketPool = _.concat(socketPool, conn)
 }
 
-const readState = (partyId, callback) => {
-  client.get(partyId, callback)
+const leaveParty = (conn) => {
+  socketPool = _.reject(socketPool, (c) => c === conn)
 }
 
-const mutateState = (partyId, reducer) => {
-  readState(partyId, (err, reply) => {
-    let newState = reducer(JSON.parse(reply))
-    console.log('state mutated')
-    client.set(partyId, JSON.stringify(newState))
+const publishAction = (action) => {
+  let connections = _.filter(socketPool, (conn) => conn.partyId == action.partyId)
+  console.log('bouncing to '+connections.length+' connections')
+  _.each(connections, (conn) => {
+    conn.send(JSON.stringify(action))
   })
 }
 
-const voteAction = (action) => {
-  mutateState(action.partyId, (votes) => {
-    let {uuid, userId, userName, partyId, points} = action
-    let newState = [
-      ..._.reject(votes, (v) => v.userId == action.userId),
-      {uuid, userName, userId, partyId, points}
-    ]
-    updateClients(partyId, newState)
-    return newState
-  })
-}
-
-const leaveParty = (conn, action) => {
-  console.log('connection '+conn.key+' leaving party')
-  socketPool[action.uuid] = _.reject(socketPool[action.uuid], (c) => c === conn)
-}
-
-const SyncVote = (votes) => ({
-  type: 'SYNC_VOTE',
-  votes
-})
-
-const cleanupDeadConnections = (partyId) => {
-  socketPool[partyId] = _.filter(socketPool[partyId], (c) => c.readyState === 1)
-}
-
-const updateClients = (partyId, state) => {
-  cleanupDeadConnections(partyId)
-  _.each(socketPool[partyId], (conn) => {
-    console.log('updating conn '+conn.key+' for party '+partyId)
-    conn.send(JSON.stringify(SyncVote(state)))
-  })
-}
-
-let server = ws.createServer(function(conn) {
+let server = ws.createServer((conn) => {
   console.log('new connection')
 
   conn.on('text', (str) => {
     let action = JSON.parse(str)
     console.log('incoming action: '+ action.type)
     switch(action.type) {
-      case "VOTE":
-        voteAction(action)
-        break
-      case "CREATE_PARTY":
-        break
       case "JOIN_PARTY":
-        console.log(action)
-        joinAction(conn, action)
+        joinParty(conn, action)
         break
       case 'LEAVE_PARTY':
         leaveParty(conn, action)
         break
     }
+    publishAction(action)
   })
 
-  conn.on('close', () => console.log('connection closed'))
+  conn.on('close', () => leaveParty(conn))
 
   conn.on('error', (err) => {})
 }).listen(8001)
